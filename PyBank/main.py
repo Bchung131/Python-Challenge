5b1742e7f337672dc233f46384f2e92034d09d57
Bchung131 <brian13196@gmail.com>
2023-10-18 14:29:51 -0400

    Code for main.py added

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,3 +8,6 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     print(csvreader)
+
+    csv_header =(next(csvreader))
+    print(f"CSV Header: {csv_header}")
