da3bf549fb0815bdad0f05e3544808bfaff88937
Bchung131 <brian13196@gmail.com>
2023-10-22 22:08:16 -0400

    Adding the script to github.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,8 +16,8 @@ with open(csvpath) as csvfile:
     DG_votes = 0
     RAD_votes = 0
     total_votes = 0
+    winner = []
 
-    
     for row in csvreader: 
       
       #Counts the total votes.
@@ -31,23 +31,49 @@ with open(csvpath) as csvfile:
       elif row[2] == "Raymon Anthony Doane":
         RAD_votes += 1
     
-   #Testing my variables and values
-    print(CCS_votes)
-    print(DG_votes)
-    print(RAD_votes)
+    #Calcuating the voting percentage for each candidate.
     CCS_vote_percentage = ((CCS_votes/total_votes)*100)
     DG_vote_percentage = ((DG_votes/total_votes)*100)
     RAD_vote_percentage = ((RAD_votes/total_votes)*100)
 
-    print(f"{CCS_vote_percentage:.3f}")
-    print(f"{DG_vote_percentage:.3f}")
-    print(f"{RAD_vote_percentage:.3f}")
+    total_results = [CCS_votes, DG_votes, RAD_votes]
+
+    #Prints out the winner.
+    if CCS_votes == max(total_results):
+      winner = "Charles Casper Stockham"
+    elif DG_votes == max(total_results):
+      winner = "Diana DeGette"
+    elif RAD_votes == max(total_results):
+      winner = "Raymon Anthony Doane"
 
 #Prints out the voting results.
 print("Election Results")
 print("--------------------------")
-print(f'Total Votes : {total_votes}')
+print(f'Total Votes: {total_votes}')
 print("--------------------------")
-print()
+print(f'Charles Casper Stockham: {f"{CCS_vote_percentage:.3f}"}% ({CCS_votes})')
+print(f'Diana DeGette: {f"{DG_vote_percentage:.3f}"}% ({DG_votes})')
+print(f'Raymon Anthony Doane: {f"{RAD_vote_percentage:.3f}"}% ({RAD_votes})')
 print("--------------------------")
-   
+print(f'Winner: {winner}')
+print("--------------------------")
+
+#Creates an outpath to a text file.
+csv_output_path = os.path.join("analysis", "Election Results")
+  
+with open(csv_output_path, 'w') as csvfile:
+
+    csvwriter = csv.writer(csvfile,)
+
+  #Writes election results to a text file.
+
+    csvwriter.writerow(["Election Results"])
+    csvwriter.writerow(["--------------------------"])
+    csvwriter.writerow([f'Total Votes: {total_votes}'])
+    csvwriter.writerow(["--------------------------"])
+    csvwriter.writerow([f'Charles Casper Stockham: {f"{CCS_vote_percentage:.3f}"}% ({CCS_votes})'])
+    csvwriter.writerow([f'Diana DeGette: {f"{DG_vote_percentage:.3f}"}% ({DG_votes})'])
+    csvwriter.writerow([f'Raymon Anthony Doane: {f"{RAD_vote_percentage:.3f}"}% ({RAD_votes})'])
+    csvwriter.writerow(["--------------------------"])
+    csvwriter.writerow([f'Winner: {winner}'])
+    csvwriter.writerow(["--------------------------"])
